39e611e52398d5f3b71a33f3c67ad99dd256fe2f
Naveen <naveenramlu@gmail.com>
2025-01-23 18:55:31 +0530

    Add the project

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import the app module
+const app = require('./src/app');
+
+// Set the port
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
